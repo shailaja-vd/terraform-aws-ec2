807164b5f48960d8d6e47811a23475e5327c5a64
shailajae <shailajaenjam0@gmail.com>
2026-06-14 17:16:29 +0530

    terraform

--- variables.tf
+++ variables.tf
@@ -9,7 +9,11 @@ variable "sg_id" {
 }
 
 variable "instance_type" {
-  default = "t3.micro"
+    default = "t3.micro"
+    validation {
+        condition     = contains(["t3.micro", "t3.small", "t3.medium"], var.instance_type)
+        error_message = "Valid values for instance type are: t3.small t3.medium t3.micro"
+    } 
 }
 
 # optional
